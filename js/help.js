f971b24053d0cda61a53c1674a8812652379755f
ayrtonaoki <ayrtonsasouza@gmail.com>
2018-05-15 15:47:41 -0300

    First commit

---
+++ js/help.js
@@ -0,0 +1,21 @@
+function help(level){
+	switch(level) {
+		case 0:
+			alert("Click here to get some tips about the questions...");
+			break;
+		case 1:
+			alert("Can't type 'something'?");
+			break;
+		case 2:
+			alert("Seems like the cursor changes in text areas...")
+			break;
+		case 3:
+			alert("Words appear when you hover over them...");
+			break;
+		case 4:
+			alert("Did you notice any difference in the adress bar?");
+		break;
+		case 5:
+			alert("Seems like the answers are connected...")	
+	}
+}
---
+++ js/validate.js
@@ -0,0 +1,45 @@
+function validateForm(level) {
+	// GETTING THE ANSWER FIELD AND VALIDATING
+	var answer = document.getElementById("frmAnswer");
+
+	switch(level) {
+		case 1:
+			if (answer.value == "something")
+				return true;
+			else {
+				answer.value = "";
+				return false;
+			}
+			break;
+		case 2:
+			if (answer.value == "happened")
+				return true;
+			else {
+				answer.value = "";
+				return false;
+			}
+			break;
+		case 3:
+			if (answer.value == "longtimeago")
+				return true;
+			else {
+				answer.value = "";
+				return false;
+			}
+			break;
+		case 4:
+			if (answer.value == "ineedyourhelp")
+				return true;
+			else {
+				answer.value = "";
+				return false;
+			}
+		case 5:
+			if (answer.value == "something happened long time ago i need your help")
+				return true;
+			else {
+				answer.value = "";
+				return false;
+			}						
+	}
+}
