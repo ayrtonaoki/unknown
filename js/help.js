3e69d9514a05a7ff788d7e8e9a138a2e18c121dc
ayrtonaoki <ayrtonsasouza@gmail.com>
2024-09-29 16:34:03 -0300

    Change some help messages

--- js/help.js
+++ js/help.js
@@ -4,10 +4,10 @@ function help(level){
 			alert("Click here to get some tips about the questions...");
 			break;
 		case 1:
-			alert("Can't type 'something'?");
+			alert("Something is the answer...");
 			break;
 		case 2:
-			alert("Seems like the cursor changes in text areas...")
+			alert("Selecting text may help...")
 			break;
 		case 3:
 			alert("Words appear when you hover over them...");
@@ -16,6 +16,6 @@ function help(level){
 			alert("Did you notice any difference in the adress bar?");
 		break;
 		case 5:
-			alert("Seems like the answers are connected...")	
+			alert("Perhaps the previous answers form a message...")
 	}
 }
